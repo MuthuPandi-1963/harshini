e0e987c6bef05bb7b8626dbdefe1a1896ca09aad
MuthuPandi-1963 <muthupandir74738@gmail.com>
2025-10-07 15:28:46 +0530

    datatypes,input

---
+++ basics/learn.java
@@ -0,0 +1,32 @@
+package basics;
+
+import java.util.Scanner;
+
+public class learn {
+    public static void main(String[] args) {
+        System.out.print("Hello world \n");
+        byte b = -128;
+        short s = 3200;
+        int n = Integer.MAX_VALUE ;
+
+        double powers = Math.pow(8, 3);
+        System.out.println(b);
+        System.out.println(n);
+        System.out.println(powers);
+
+        char c = 'd';
+        String a = "apple";
+        System.out.println(a.charAt(4));
+
+        boolean isActive = true;
+        isActive = false;
+
+        Scanner scan = new Scanner(System.in);
+        System.out.print("enter a name :");
+        String name = scan.nextLine();
+
+        System.out.println("Welcome , "+name );
+
+
+    }
+}
