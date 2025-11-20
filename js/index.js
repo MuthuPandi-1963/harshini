c600865b75ecf39dfe2c8aff72a8914e6cd95869
MuthuPandi-1963 <muthupandir74738@gmail.com>
2025-11-20 14:48:57 +0530

    done

---
+++ js/index.js
@@ -0,0 +1,45 @@
+console.log("Welcome external js");
+"https://chatgpt.com/share/691edc47-77cc-800c-9f2d-264760d42961"
+// let or const
+
+let a ="10"
+let b = 10
+console.log(typeof a , typeof b);
+
+console.log(a == b);
+console.log(a === b);
+console.log(a !== b);
+
+
+const x = ""
+const y = 0
+const z = false
+
+const m = " " 
+let n = 1
+n+=10
+const o = true
+
+const ok = null
+let yes; 
+// ++a
+console.log(n && "fine");
+
+
+console.log(10 == 10 && "its equal");
+console.log(10 != 10 || "its equal");
+console.log(null && "its null");
+console.log(null || "its null");
+
+console.log(10 == 10 ?? "its equal");
+console.log(null ?? "its equal");
+console.log(undefined ?? "its equal");
+console.log(false ?? "its equal");
+
+console.log("5"+3);
+console.log("5"-3);
+console.log(Math.floor(30/7));
+
+
+const [q,w,e] = [10,20,30]
+console.log(q,w,e);
